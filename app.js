db0a20b37609050694d89952cffb2ff568418fa5
Axolism <ideunu108@gmail.com>
2023-01-29 16:00:34 -0800

    added colorchange

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+const colorOptions = Array.from(
+    document.getElementsByClassName("color-options")
+)
+
 const lineWidth = document.getElementById("line-width")
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const color = document.getElementById("color");
 canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = lineWidth.value;
@@ -28,13 +33,28 @@ function onLineWidthchange(event) {
  ctx.lineWidth=event.target.value;
 }
 
-canvas.addEventListener("mousemove", onMove)
-canvas.addEventListener("mousedown", startPainting)
-canvas.addEventListener("mouseup", cancelPainting)
-canvas.addEventListener("mouseleave", cancelPainting)
-lineWidth.addEventListener("change", onLineWidthchange)
+
+function onColorChange(event) {
+    ctx. strokeStyle = event.target.value;
+    ctx.fillStyle = event.target.value;
+}
+
+function onColorClick(event){
+    const colorValue = event.target.dataset.color;
+    ctx.strokeStyle = colorValue;
+    ctx.fillStyle = colorValue;
+    color.value = colorValue
+}
 
 
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+color.addEventListener("change", onColorChange);
+lineWidth.addEventListener("change", onLineWidthchange);
+
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick))
 
 
 
