e34b6303378c1aa3d0366417806b734c0aece6c4
Axolism <ideunu108@gmail.com>
2023-02-05 15:19:02 -0800

    technical changes

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const modeBtn = document.getElementById("mode-btn")
+const destroyBtn = document.getElementById("destroy-btn")
+const eraserBtn = document.getElementById("eraser-btn")
 const colorOptions = Array.from(
     document.getElementsByClassName("color-options")
 )
@@ -6,10 +9,13 @@ const lineWidth = document.getElementById("line-width")
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const color = document.getElementById("color");
-canvas.width = 800;
-canvas.height = 800;
+const canvas_width = 800;
+const canvas_height = 800;
+canvas.width = canvas_width;
+canvas.height = canvas_height;
 ctx.lineWidth = lineWidth.value;
 let isPainting = false;
+let isFilling = false;
 
 function onMove(event){
  if(isPainting) {
@@ -46,15 +52,43 @@ function onColorClick(event){
     color.value = colorValue
 }
 
+function onModeClick () {
+    if (isFilling) {
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+    } else {
+        isFilling = true;
+        modeBtn.innerText ="Draw"
+    }
+}
+
+function onCanvasClick() {
+    if (isFilling) {
+        ctx.fillRect (0, 0,canvas_width ,canvas_height );
+    }
+}
+function onDestroyClick() {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0,0, canvas_width, canvas_height);
+}
+
+function onEraserClick( ) {
+    ctx.strokeStyle = "white";
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+}
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click",onCanvasClick),
 color.addEventListener("change", onColorChange);
 lineWidth.addEventListener("change", onLineWidthchange);
 
 colorOptions.forEach((color) => color.addEventListener("click", onColorClick))
+modeBtn.addEventListener("click", onModeClick);
 
 
-
+destroyBtn.addEventListener("click", onDestroyClick);
+eraserBtn.addEventListener("click", onEraserClick);
